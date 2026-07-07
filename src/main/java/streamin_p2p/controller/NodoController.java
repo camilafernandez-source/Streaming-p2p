326cc175470e97389443ff9e5fabbadc6b8d3f1b
Camila Alejandra Fernández Torres <camilaalejandrafernandeztorres@Karlas-MacBook-Air.local>
2026-07-06 23:12:59 -0600

    implementacion mecanismo Pub/Sub y logica de descarga

---
+++ src/main/java/streamin_p2p/controller/NodoController.java
@@ -0,0 +1,34 @@
+package streamin_p2p.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import streamin_p2p.model.Fragmento;
+import streamin_p2p.service.NodoService;
+
+@RestController
+@RequestMapping("/api/p2p")
+
+public class NodoController {
+
+    private final NodoService nodoService;
+
+    public NodoController(NodoService nodoService){
+        this.nodoService=nodoService;
+    }
+
+    @GetMapping("/simular-descarga")
+    public String simularDescarga(@RequestParam String nodo, @RequestParam int idFragmento){
+        //simulamos con bytes de texto
+        byte[] contenidoFalso = "pedacito_de_mp4".getBytes();
+        Fragmento nuevoFragmento = new Fragmento(idFragmento, contenidoFalso, nodo);
+
+        //registramos el fragmento
+        nodoService.registrarFragmento(nodo, nuevoFragmento);
+
+        return "El nodo " + nodo + " aviso a la red que tiene el fragmento " + idFragmento;
+    }
+    
+}
--- src/main/java/streamin_p2p/service/NodoService.java
+++ src/main/java/streamin_p2p/service/NodoService.java
@@ -1,29 +1,64 @@
 package streamin_p2p.service;
 
-import streamin_p2p.model.Fragmento;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 import streamin_p2p.event.FragmentoDisponibleEvent;
+import streamin_p2p.model.Fragmento;
 
 @Service
 public class NodoService {
     private final ApplicationEventPublisher eventPublisher; //almacenamiento de fragmentos
 
+    //lista de nodos
     private final Map<String, Map<Integer, Fragmento>> storage = new HashMap<>();
+
+    private final List<String> nodosEnLaRed = Arrays.asList("NodoA", "NodoB", "NodoC");
 
     public NodoService(ApplicationEventPublisher eventPublisher){
         this.eventPublisher = eventPublisher;
+        //nodos con mapas vacios
+        for (String nodo : nodosEnLaRed){
+            storage.put(nodo, new HashMap<>());
+        }
     }
 
     public void registrarFragmento(String nodoId, Fragmento frag){
-        storage.computeIfAbsent(nodoId, k -> new HashMap<>()).put(frag.getId(), frag);
-        System.out.println("Nodo " + nodoId + "adquirio el fragmento" + frag.getId());
+        storage.get(nodoId).put(frag.getId(), frag);
+        System.out.println(nodoId + " descargo el fragmento " + frag.getId());
 
-        // Publicar el evento de fragmento disponible
+        //nodo le avisa a todos los demas
         eventPublisher.publishEvent(new FragmentoDisponibleEvent(this, frag));
+    }
+
+    @EventListener
+    //avisa cuando un nodo consigue un fragmento
+    public void escucharRed(FragmentoDisponibleEvent event){
+        Fragmento fragAnunciado = event.getFragmento();
+        String nodoQueLoTiene = fragAnunciado.getNodoOrigen();
+        int idDelFragmento = fragAnunciado.getId();
+
+        System.out.println("[Alerta P2P] " + nodoQueLoTiene + " anuncio que tiene el fragmento " + idDelFragmento);
+
+        //los demas nodos revisan si les falta el fragmento
+        for (String otroNodo : nodosEnLaRed){
+            if (!otroNodo.equals(nodoQueLoTiene)){
+                //si no esta el fragmento en el nodo, se lo descarga
+                if (!storage.get(otroNodo).containsKey(idDelFragmento)){
+                    System.out.println(otroNodo + " anuncia que le falta el fragmento " + idDelFragmento + ", solicitando descarga desde " + nodoQueLoTiene);
+
+                    //simulacion de la descarga
+                    Fragmento copiaDescarga = new Fragmento(idDelFragmento, fragAnunciado.getContenido(), otroNodo);
+                    storage.get(otroNodo).put(idDelFragmento, copiaDescarga);
+                    System.out.println(otroNodo + " termino de descargar el fragmento " + idDelFragmento);
+                }
+            }
+        }
     }
 }
