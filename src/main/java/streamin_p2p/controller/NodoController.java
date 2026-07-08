ccda4dab5da22ba130d99cae447eda6bc377f724
Camila Alejandra Fernández Torres <camilaalejandrafernandeztorres@Karlas-MacBook-Air.local>
2026-07-08 11:46:50 -0600

    se añadio swagger OpenaPI

--- src/main/java/streamin_p2p/controller/NodoController.java
+++ src/main/java/streamin_p2p/controller/NodoController.java
@@ -1,5 +1,7 @@
 package streamin_p2p.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
